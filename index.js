6c90015ce73be3bf9f615e9643bec885364babb8
arifsantoso <arif.santoso@bithealth.co.id>
2024-10-11 20:26:58 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// const pola = /java script/
+// const value = "Belajar java Script"
+
+// const data = [1,2,3, 2]
+
+// console.log(pola.test(value));
+
+// console.log(data.find(i => i === 2));
+// console.log(data.filter(i => i === 2));
+
+// const pola2 = /a/gi
+// const value = "apa Asih 1 ss 3"
+
+// let result 
+
+// do {
+//   result = pola2.exec(value)
+
+//   console.log(result);
+  
+// } while (result);
+
+// username@domain.com
+
+// const emailPola = /[a-z]\d+#+@+gmail+\.[a-z]/
+// const email = "arifd12#@gmail.com"
+
+// console.log(emailPola.test(email));
+
+
+let age = 20
+
+age += 10
+
+console.log(age);
+
+
+
